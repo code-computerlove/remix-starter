d745cba6e7df6abf13d6ce6b856ddab4be493e39
Alex Hall <alex.hall@codecomputerlove.com>
2023-05-23 10:16:21 +0100

    fix: fixed the init script trying to remove files that don't exist (#137)

--- remix.config.js
+++ remix.config.js
@@ -5,10 +5,12 @@ module.exports = {
 	cacheDirectory: './node_modules/.cache/remix',
 	ignoredRouteFiles: ['**/.*', '**/*.css', '**/*.test.{js,jsx,ts,tsx}'],
 	future: {
-		unstable_cssModules: true,
 		v2_routeConvention: true,
 		v2_errorBoundary: true,
 		v2_meta: true,
+		v2_normalizeFormMethod: true,
 	},
 	serverDependenciesToBundle: [],
+	serverModuleFormat: 'cjs',
+	postcss: true,
 };
